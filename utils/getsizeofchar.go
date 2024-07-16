4bd535f07032d6de2cd5d628d77f8102b20318c6
antmusumba <musumbaantony3@gmail.com>
2024-07-16 10:59:58 +0300

    feat: add getSizeOfChar file

---
+++ utils/getsizeofchar.go
@@ -0,0 +1,9 @@
+package utils
+func GetSizeOfCharacters(word string,contentLines []string) int {
+	var length int
+	for _, v := range word {
+		start := ((v - 32) * 9) + 4
+		length += len(contentLines[start])
+	}
+	return length
+}
