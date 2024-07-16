63b7fddabacb58470d7bb8b17f11e88285718859
antmusumba <musumbaantony3@gmail.com>
2024-07-16 11:04:22 +0300

    chore: refactor code

--- utils/display2.go
+++ utils/display2.go
@@ -2,18 +2,12 @@ package utils
 
 import (
 	"fmt"
-	"os"
+
 	"strings"
 )
 
 func DisplayText2(input string, contentLines []string) string{
-	if input == "" {
-		os.Exit(0)
-	}
-	if input == "\\n" || input == "\n" {
-		fmt.Println()
-		os.Exit(0)
-	}
+	
 	dat2 := ""
 	
 	// split the input string with the "\\n" into a slice strings
@@ -34,7 +28,7 @@ func DisplayText2(input string, contentLines []string) string{
 			linesOfSlice[i-1] += contentLines[int(v-32)*9+i]
 		}
 	}
-	dat2 = (strings.Join(linesOfSlice, "\n"))
+	dat2 += (strings.Join(linesOfSlice, "\n"))
 
 		}
 	}
